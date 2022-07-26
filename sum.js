51114fb948ce324ffd20f1cae1306a6146c84ab6
Dominic Tremblay <dominictremblay24@gmail.com>
2022-07-26 12:37:01 -0400

    refacotring sum with helper functions

--- sum.js
+++ sum.js
@@ -1,53 +1,74 @@
 // - Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
 
-
 // function?
-// for loop 
-
+// for loop
 
 // Extract the command line arguments
 const args = process.argv.slice(2);
-console.log("args:", args);
+console.log('args:', args);
 
 // Edge case: We need at least 2 arguments.
 if (args.length < 2) {
-  console.log("Please enter at least 2 arguments!")
+  console.log('Please enter at least 2 arguments!');
   process.exit();
 }
 
+const convertToNum = function (numbers) {
+  const result = [];
 
-// iterate through the arguments
-// c-style loop 
-// for of <= more readable
-
-// accumulator
-let total = 0;
-
-for (let nb of args) {
-  // add them up
-  // Edge case: If any argument is not a number, output an error message
-  // isNaN?
-  // typeof?
-
-  if (isNaN(Number(nb))) {
-    console.log("Please enter only numbers!")
-    // break or continue
-    // return?
-    process.exit()
-  }
-  // Edge case: If any argument is not a whole number, skip it
-  // modulo => nb % 1 === 0 => whole number
-  // Number.isInteger()
-
-  if (Number.isInteger(Number(nb))) {
-    total += Number(nb);
+  for (let nb of numbers) {
+    result.push(Number(nb));
   }
 
-  console.log("total:", total, typeof Number(nb))
-}
+  return result;
+};
+
+const allInt = function (numbers) {
+  const result = [];
+
+  for (let nb of numbers) {
+    if (Number.isInteger(nb)) {
+      result.push(nb);
+    }
+  }
+
+  return result;
+};
+
+const sum = function (numbers) {
+  // iterate through the arguments
+  // c-style loop
+  // for of <= more readable
+
+  // accumulator
+  let total = 0;
+
+  for (let nb of numbers) {
+    // add them up
+    // Edge case: If any argument is not a number, output an error message
+    // isNaN?
+    // typeof?
 
 
 
+    if (isNaN(nb)) {
+      console.log('Please enter only numbers!');
+      // break or continue
+      // return?
+      process.exit();
+    }
+
+    total += nb;
+
+
+    // Edge case: If any argument is not a whole number, skip it
+    // modulo => nb % 1 === 0 => whole number
+    // Number.isInteger()
+  }
+  return total;
+};
+
+const result = sum(allInt(convertToNum(args)));
 
 // print out the sum
-console.log("Sum:", total)
+console.log('Sum:', result);
